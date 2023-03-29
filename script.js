ffebe7669fb768ca37c8ae251f25377d6ec46ab0
DmytroHladkyi <dmytrogladkyi8@gamil.com>
2023-03-29 08:58:08 +0200

    animated_CV

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multipel-text", {
+    strings: ["beginner developer", "self-taught", ""],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
